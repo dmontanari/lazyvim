ad93db4226d28126980c18b705e107a91e4cfd9a
Cabeca <dmontanari@gmail.com>
2024-08-02 23:32:23 -0300

    <C-p>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Telescope find and grep
 local builtin = require("telescope.builtin")
--- vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
 -- NeoTree
