ac3dd3f73d4b28a4f8f2c7acc6306554d8e9307e
cabeca <cabeca@dev>
2024-08-26 23:17:26 -0300

    tabs

--- init.lua
+++ init.lua
@@ -1,22 +1,17 @@
--- Configura a largura do tab para 4 espaços
-vim.o.tabstop = 4
-
--- Configura a largura da indentação para 4 espaços
-vim.o.shiftwidth = 4
-
--- Faz com que o pressionar de Tab insira espaços em vez de tabs
-vim.o.expandtab = true
-
 -- Comandos personalizados para habilitar/desabilitar o IntelliSense
 vim.api.nvim_create_user_command("IntelliOff", function()
-  require("cmp").setup({ enabled = false })
+    require("cmp").setup({ enabled = false })
 end, {})
 
 vim.api.nvim_create_user_command("IntelliOn", function()
-  require("cmp").setup({ enabled = true })
+    require("cmp").setup({ enabled = true })
 end, {})
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
 require("lspconfig").clangd.setup({})
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
